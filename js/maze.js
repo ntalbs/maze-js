193afc0b43c62746afa5c8c128b42a1857748864
ntalbs <ntalbsen@gmail.com>
2015-08-04 08:39:35 +0900

    미로 생성 알고리즘 함수를 독립 함수로...

--- js/maze.js
+++ js/maze.js
@@ -5,27 +5,28 @@ function rand(limit) {
   return Math.floor(Math.random() * limit);
 }
 
+function binaryTree (rows, cols) {
+  var grid = new Grid(rows, cols);
+  grid.eachCell().forEach(function (cell) {
+    var neighbors = [];
+
+    var north = grid.northOf(cell);
+    if (north) neighbors.push(north);
+
+    var east = grid.eastOf(cell);
+    if (east) neighbors.push(east);
+
+    if (neighbors.length > 0) {
+      var index = rand(neighbors.length);
+      cell.link(neighbors[index]);
+    }
+  });
+  return grid;
+}
+
 function maze(algorithm) {
   switch(algorithm) {
-  case 'binaryTree' :
-    return function (rows, cols) {
-      var grid = new Grid(rows, cols);
-      grid.eachCell().forEach(function (cell) {
-        var neighbors = [];
-
-        var north = grid.northOf(cell);
-        if (north) neighbors.push(north);
-
-        var east = grid.eastOf(cell);
-        if (east) neighbors.push(east);
-
-        if (neighbors.length > 0) {
-          var index = rand(neighbors.length);
-          cell.link(neighbors[index]);
-        }
-      });
-      return grid;
-    };
+  case 'binaryTree': return binaryTree;
   default:
     throw new 'not supported algorithm';
   }
