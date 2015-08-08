44ea201dd5f16902f81e50b6d8c4f9e800785403
ntalbs <ntalbsen@gmail.com>
2015-08-08 17:25:59 +0900

    코드 포매팅 (공백 조정)

--- gulpfile.js
+++ gulpfile.js
@@ -22,19 +22,19 @@ var b = watchify(browserify(opts));
 // i.e. b.transform(coffeeify);
 
 gulp.task('js', bundle); // so you can run `gulp js` to build the file
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
+b.on('update', bundle);  // on any dep update, runs the bundler
+b.on('log', gutil.log);  // output build logs to terminal
 
 function bundle() {
   return b.bundle()
-    // log errors if they happen
+  // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
-    // optional, remove if you don't need to buffer file contents
+  // optional, remove if you don't need to buffer file contents
     .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
+  // optional, remove if you dont want sourcemaps
     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-       // Add transformation tasks to the pipeline here.
+  // Add transformation tasks to the pipeline here.
     .pipe(sourcemaps.write('./')) // writes .map file
     .pipe(gulp.dest('./dist'));
 }
